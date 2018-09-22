4b5c1adfaeda7e987b0672dd3083f87c74366414
William Li <7bobperson7@gmail.com>
2018-09-22 04:50:20 -0400

    m for message

---
+++ PongGame/app/src/main/java/w/pongrender/Sprite.java
@@ -0,0 +1,33 @@
+package w.pongrender;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+import java.util.ArrayList;
+
+public class Sprite {
+    private Bitmap image;
+    private int x,y;
+
+    public Sprite(Bitmap bmp) {
+        image = bmp;
+        x=0;
+        y=0;
+    }
+    public Sprite(Bitmap bmp,int inx, int iny) {
+        image = bmp;
+        x=inx;
+        y=iny;
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(image, x, y, null);
+    }
+    public void update(){
+//        int[] coords = Oliver.renderBall(dimenx, dimeny);
+//        x = coords[0];
+//        y = coords[1];
+        x=x+1;
+        y=y+1;
+    }
+}
